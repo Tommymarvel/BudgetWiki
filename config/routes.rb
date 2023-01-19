376aea792c60a9596458e7b3c4bdd34063c12044
Tommymarvel <ibironketomiwa4@gmail.com>
2023-01-19 10:30:02 +0100

    Added contoller, model and necessary gems

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+Rails.application.routes.draw do
+  resources :categories
+  resources :entities
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
